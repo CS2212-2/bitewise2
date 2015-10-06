77b75574fee657bfef1afcd655e1e301e3682560
alsmadi <alsmadi@gmail.com>
2015-10-05 21:00:04 -0700

    initial commit

---
+++ Debug/src/Bit20.d
@@ -0,0 +1,1 @@
+src/Bit20.d: ../src/Bit20.cpp
---
+++ src/Bit20.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int countSetBits(unsigned int n) {
+  unsigned int count = 0;
+  while(n)   {
+printf("output is %d, %d \n", n, count);
+    count += n & 1;
+    n >>= 1;
+  }
+  return count;
+}
+
+/* Program to test function countSetBits */
+int main() {
+int myvariable;
+printf("Enter a number:");
+scanf("%d",&myvariable);
+printf("output is %d \n", countSetBits(myvariable));
+return 0;
+}
